43425d5a20398b6cce2beed71ab57ff7764111ef
davidmay75 <davidmay75@users.noreply.github.com>
2021-10-11 11:25:32 -0400

    Link to google map for tracks

--- src/Components/Circuit.js
+++ src/Components/Circuit.js
@@ -12,7 +12,8 @@ const Circuit = ({ circuitId, circuitName, url, Location }) => {
               <div className="row">City: {Location.locality}</div>
               <div className="row">Country: {Location.country}</div>
               <div className="row">
-                Location: {Location.lat} {Location.long}
+              Location: <a href={"http://maps.google.com/maps?t=k&q="+Location.lat+","+Location.long}>{Location.lat}, {Location.long}</a>
+
               </div>
             </div>
         </Card.Body>
